b51c94b6c560b5f914a47a6f05e3a906666704e6
Ava Zheng <zz1456@nyu.edu>
2020-03-29 19:25:25 -0700

    modified player, bullet, map, and game structure

--- Travel Back In Myth/Assets/Scritps/playerController.cs
+++ Travel Back In Myth/Assets/Scritps/playerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class playerController : MonoBehaviour
 {
@@ -15,16 +16,22 @@ public class playerController : MonoBehaviour
     public Transform firePoint;
     public GameObject bulletPrefab;
     Vector2 bulletPos;
-    public float fireRate = 0.5f;
+    public float fireRate = 0.1f;
     float nextFire = 0.0f;
+
+    private Scene scene;
+    int lvl = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        lvl = SceneManager.GetActiveScene().buildIndex;
         face_right = true;
         startPos = transform.position;
 
         rb = GetComponent<Rigidbody2D>();
+
+        scene = SceneManager.GetActiveScene();
     }
 
 
@@ -33,25 +40,27 @@ public class playerController : MonoBehaviour
     {
         move = Vector3.zero;
 
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            jump = true;
-        }
-
         if (Input.GetKey(KeyCode.D))
         {
             move += transform.right;
             if (face_right != true) { flip(); }
 
         }
-        else if (Input.GetKey(KeyCode.A))
+        if (Input.GetKey(KeyCode.A))
         {
             move -= transform.right;
             if (face_right == true) { flip(); }
         }
+        if (Input.GetKey(KeyCode.W))
+        {
+            move += transform.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            move -= transform.up;
+        }
 
-        if (Input.GetButtonDown("Fire1") && Time.time > nextFire)
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextFire)
         {
             nextFire = Time.time + fireRate;
             fire();
@@ -80,7 +89,19 @@ public class playerController : MonoBehaviour
             jump = false;
             rb.AddForce(transform.up * 6, ForceMode2D.Impulse);
         }
-        rb.AddForce(move * 10);
+        rb.AddForce(move * 8);
 
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "enemy")
+        {
+            Application.LoadLevel(scene.name);
+        }
+        if (collision.gameObject.tag == "Key")
+        {
+            lvl += 1;
+            SceneManager.LoadScene(lvl);
+        }
     }
 }
